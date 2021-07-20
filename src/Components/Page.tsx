eb9241f81a71b5a9e1f1c0b0398a48c77bfe1f4a
Yannick <y.alsberge@yalit.be>
2021-07-20 15:02:34 +0200

    feat : update Page display

--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {RefObject, TouchEvent, useRef, useState} from "react";
+import Swipeable from 'react-native-gesture-handler';
 import {Page} from "../Helpers/Page";
 import './Styles/page.scss';
 
-type PageSmallProps = {
+type PageDisplayProps = {
     page: Page
 }
 
-export const PageSmall:React.FC<PageSmallProps> = ({page}) => {
+export const PageDisplay:React.FC<PageDisplayProps> = ({page}) => {
+    let pageRef:RefObject<HTMLDivElement> = useRef(null);
+    let [displayMoveAction, useDisplayMoveAction] = useState<string|null>(null)
+    let touchStartX: number | undefined;
+    const imgStyle = {
+        backgroundImage: `url(${page.img})`
+    }
+
+
+
     return (
-        <div className={'page__small'}>
-            <div className="page__small__title">{page.title}</div>
+        <div ref={pageRef} className={'page__display'} >
+            {displayMoveAction && displayMoveAction === 'edit' && <div className={'page__action__delete_button'}>Edit</div>}
+            <div className="page__display__img" style={imgStyle}></div>
+            <div className="page__display__content">
+                <div className="page__display__title">{page.title}</div>
+                <div className="page__display__description">{page.description}</div>
+            </div>
         </div>
     )
 }
